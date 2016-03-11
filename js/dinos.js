a0dea1c5f3939665f6ba8c9f70002ff73f3bf333
kaylaboch <boch.kayla57@gmail.com>
2016-03-11 16:05:48 -0500

    added files

---
+++ js/dinos.js
@@ -0,0 +1,14 @@
+var dinos = [
+  {
+    name: 'Diplodocus',
+    img: 'diplodocus.jpg',
+  },
+  {
+    name: 'Hadrosaur',
+    img: 'hadrosaur',
+  },
+  {
+    name: 'Iguanodon',
+    img: 'iguanodon.jpg',
+  }
+];
